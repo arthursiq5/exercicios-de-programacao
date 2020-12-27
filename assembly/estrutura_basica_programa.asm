f793b83367cd62f88a7b57f373ac56019d1e84c6
arthursiq5 <arthur.siq5@gmail.com>
2020-12-27 16:29:27 -0300

    estrutura básica de programa em assembly

---
+++ assembly/estrutura_basica_programa.asm
@@ -0,0 +1,13 @@
+section .dat
+    ;declaração e inicialização de dados
+
+section .bss
+    ;declaração sem inicialização das informações que serão usadas
+
+section .text
+
+global _start
+
+_start
+    ;o programa começa a rodar a partir daqui
+
