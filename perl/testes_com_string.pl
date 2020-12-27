703f415f49b79114aed66138c5e7100640dcb031
arthursiq5 <arthur.siq5@gmail.com>
2020-12-27 00:41:36 -0300

    alguns testes com string em perl

---
+++ perl/testes_com_string.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+use v5.010;
+
+say 'Curso Perl Progressivo \n'; # escreve tudo exatamente como está
+say '\\ \'';
+
+say "Perl" . "Progressivo";
+say "Perl" . " " . "Progressivo";
+
+say "Rush" x 3;
+say 4 x 3;
+
