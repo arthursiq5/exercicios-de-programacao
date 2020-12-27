c7bc8be782e1b316e56c2839ea67772abef911b5
arthursiq5 <arthur.siq5@gmail.com>
2020-12-27 00:02:33 -0300

    adicionados testes com operações matemáticas em perl

---
+++ perl/operacoes_matematicas.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/perl
+print 21 + 12, "\n"; # Adição
+print 21 - 12, "\n"; # Subtração
+print 21 * 12, "\n"; # Multiplicação
+print 21 / 12, "\n"; # Divisão
+print 21 **12, "\n"; # Exponenciação
+print 21 % 12, "\n"; # Resto da divisão
