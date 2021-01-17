c69de25b4e24d5b22647a557d8942f8d295870aa
arthursiq5 <arthur.siq5@gmail.com>
2021-01-17 01:46:10 -0300

    terminada aula 3 do curso de POO com PHP

--- PHP/POO/curso-em-video/aula-02/index.php
+++ PHP/POO/curso-em-video/aula-02/index.php
@@ -10,17 +10,19 @@
 
             $caneta1 = new Caneta(); 
 
-            $caneta1->cor = "Azul";
-            $caneta1->ponta = 0.5;
-            $caneta1->tampada = false;
-            
+            $caneta1->modelo = 'BIC cristal';
+            $caneta1->cor = 'vermelho';
+
+            $caneta1->destampar();
+
             $caneta1->rabiscar();
 
             $caneta1->tampar();
 
             $caneta1->rabiscar();
 
-            print_r($caneta1)
+            print_r($caneta1);            
+
         ?>
     </bodfgdgvb  >
 </html>
