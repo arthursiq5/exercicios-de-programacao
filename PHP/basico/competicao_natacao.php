378edbfe695dfbd2a170de094c9208d40df2fdd4
arthursiq5 <arthur.siq5@gmail.com>
2021-01-23 00:10:24 -0300

    terminado exercício

--- PHP/basico/competicao_natacao.php
+++ PHP/basico/competicao_natacao.php
@@ -7,3 +7,17 @@ $categorias[] = 'adulto';
 
 print_r($categorias);
 
+$nome = 'Eduardo';
+$idade = 10;
+
+var_dump($nome);
+var_dump($idade);
+
+if ($idade >= 6 && idade <= 12) {
+    echo $categorias[0];
+} else if ($idade >=13 && $idade <=18) {
+    echo $categorias[1];
+} else {
+    echo $categorias[2];
+}
+
