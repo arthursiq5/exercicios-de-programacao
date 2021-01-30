64737fd0939ddc6e580bdee5d7d2cb27d984e164
Arthur Siqueira <arthur.siq5@gmail.com>
2021-01-30 01:11:30 -0300

    exemplo de operadores de atribuição

---
+++ PHP/basico/operadores-atribuicao.php
@@ -0,0 +1,41 @@
+<?php
+
+$var = 100;
+echo $var . '<br>';
+
+$var += 5; // soma 5
+echo $var . '<br>';
+
+$var -=5; // subtração
+echo $var . '<br>';
+
+$var *= 5; // multiplicação
+echo $var . '<br>';
+
+$var /= 5; // divisão
+echo $var . '<br>';
+
+echo '<br><br>Testes com operadores unários <br><br>';
+
+$var = 1;
+echo 'valor original: ' . $var . '<br>';
+
+echo 'Valor na utilização(++$var): ' . ++$var . '<br>';
+echo 'Valor pós utilização(++$var): ' . $var . '<hr>';
+
+$var = 1;
+
+echo 'Valor na utilização($var++): ' . $var++ . '<br>';
+echo 'Valor pós utilização($var++): ' . $var . '<hr>';
+
+$var = 1;
+
+echo 'Valor na utilização(--$var): ' . --$var . '<br>';
+echo 'Valor pós utilização(--$var): ' . $var . '<hr>';
+
+$var = 1;
+
+echo 'Valor na utilização($var--): ' . $var-- . '<br>';
+echo 'Valor pós utilização($var--): ' . $var . '<hr>';
+
+
