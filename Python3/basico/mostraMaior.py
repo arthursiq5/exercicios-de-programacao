e2b35a370665826125329c7a3bf4963766fcf6e2
Arthur Siqueira <arthur.siq5@gmail.com>
2021-01-31 23:16:52 -0300

    adicionado exemplo de uso de condicional

---
+++ Python3/basico/mostraMaior.py
@@ -0,0 +1,10 @@
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número: '))
+
+if num1 >= num2:
+    maior = num1
+else:
+    maior = num2
+
+print('O maior número digitado foi {}'.format(maior))
+
