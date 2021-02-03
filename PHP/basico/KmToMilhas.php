c879d33c54bf603588a33a74c694bef43ed16be1
Arthur Siqueira <arthur.siq5@gmail.com>
2021-02-03 08:33:00 -0300

    adicionado exercício com variáveis globais

---
+++ PHP/basico/KmToMilhas.php
@@ -0,0 +1,15 @@
+<?php
+
+$total = 0;
+
+function km2mi($quilometros)
+{
+    global $total;
+    $total += $quilometros;
+    return $quilometros * 0.6;
+}
+
+echo 'percorreu ' . km2mi(100) . " milhas <br>\n";
+echo 'percorreu ' . km2mi(100) . " milhas <br>\n";
+echo 'percorreu no total ' . $total . " quilometros<br>\n";
+
