060a365ddcf119e9ffb580016a8ac38a4ba73a33
Arthur Siqueira <arthur.siq5@gmail.com>
2021-02-07 01:25:21 -0300

    Atualizado exemplo do format

--- Python3/basico/FormatandoStrings.py
+++ Python3/basico/FormatandoStrings.py
@@ -1,3 +1,6 @@
 nums = [4, 5, 6]
 msg = "Numbers: {0} {1} {2}". format(nums[0], nums[1], nums[2]) # usar o .format() para formatar strings funciona pra atribuir em variáveis também
 print(msg)
+
+# os números entre chaves significam qual item de dentro do format será exibido
+print("{0}{1}{0}".format('abra', 'cad')) # abracadabra
