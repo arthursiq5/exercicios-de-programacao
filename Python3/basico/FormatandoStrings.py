bb823cce0955a9375a5f3827457ea7d89ae5ea54
Arthur Siqueira <arthur.siq5@gmail.com>
2021-02-07 01:28:17 -0300

    Adicionado exemplo de atribuições de variáveis em formatações

--- Python3/basico/FormatandoStrings.py
+++ Python3/basico/FormatandoStrings.py
@@ -4,3 +4,7 @@ print(msg)
 
 # os números entre chaves significam qual item de dentro do format será exibido
 print("{0}{1}{0}".format('abra', 'cad')) # abracadabra
+
+# pode setar também variáveis aos indexes
+a = "{x}, {y}".format(x=5, y=12)
+print(a)
