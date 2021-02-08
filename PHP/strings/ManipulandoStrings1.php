7c999fe659ac99bd20b9ae2e9f52e68e738f6b62
Arthur Siqueira <arthur.siq5@gmail.com>
2021-02-08 13:38:14 -0300

    adicionados testes com strings

---
+++ PHP/strings/ManipulandoStrings1.php
@@ -0,0 +1,13 @@
+<?php
+
+echo strtoupper("convertendo para maiúsculo") . '<br>' . PHP_EOL;
+echo strtolower("CONVERTENDO PARA MINÚSCULO") . '<br>' . PHP_EOL;
+
+
+
+
+
+print substr('Americana', 1) . '<br>' . PHP_EOL;
+print substr('Americana', 1, 3) . '<br>' . PHP_EOL;
+print substr('Americana', 0, -1) . '<br>' . PHP_EOL;
+print substr('Americana', -2) . '<br>' . PHP_EOL;
