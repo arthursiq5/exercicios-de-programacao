7ad9c6a5903cf875f0ed6ae584f88f288e3ecd3c
Arthur Siqueira <arthur@oneweb.com.br>
2021-02-11 20:22:24 -0300

    adicionado exemplo de uso de variáveis

---
+++ perl/basico/variaveis.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $employee_name = "Fulano"; # toda variável é inicializada com "my"
+my $employee_age = 25;
+my $employee_salary;
+$employee_salary = 998.55;
+
+print "Name   = $employee_name\n";
+print "Age    = $employee_age\n";
+print "Salary = $employee_salary\n";
