3eeb05714e989391119bd6a82fdbaa33d1f820b0
Arthur Siqueira <arthur.siq5@gmail.com>
2021-02-14 11:40:45 -0300

    Melhorada aparência visual do jogo e mensagens

--- C/Basico/adivinhacao.c
+++ C/Basico/adivinhacao.c
@@ -4,13 +4,26 @@ int main() {
     printf("************************************\n");
     printf("* Bem-vindo ao Jogo de Adivinhação *\n");
     printf("************************************\n");
+    printf("\n\n\n");
 
     int numeroSecreto = 42,
         chute = 0;
 
     printf("Qual é o seu chute? ");
     scanf(" %d", &chute);
-    printf("Você chutou o número %d!\n", chute);
+    printf("Você chutou o número %d!\n\n", chute);
+
+    if(chute == numeroSecreto){
+
+        printf("Parabéns! Você acertou!\n");
+        printf("Jogue de novo, você é muito bom\n\n");
+
+    }else{
+    
+        printf("Que pena, você errou\n");
+        printf("Não desanime, tente de novo!\n\n");
+    
+    }
 
     return 0;
 
