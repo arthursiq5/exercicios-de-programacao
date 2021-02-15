67dbcdbc19ede40263f36aa086ac56a24e7055f2
Arthur Siqueira <arthur.siq5@gmail.com>
2021-02-15 01:23:20 -0300

    adicionado sistema de colisão

--- Godot/DodgeTheCreeps/Player.gd
+++ Godot/DodgeTheCreeps/Player.gd
@@ -1,8 +1,13 @@
 extends Area2D
+signal hit
 
 export var speed = 400 # velocidade do personagem
 var screen_size
 
+func start(pos):
+    position = pos
+    show()
+    $CollisionShape2D.disabled = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -36,3 +41,10 @@ func _process(delta):
     elif velocity.y != 0:
         $AnimatedSprite.animation = "Subir"
         $AnimatedSprite.flip_v = velocity.y > 0
+
+
+func _on_Player_body_entered(body):
+    hide()
+    emit_signal("hit")
+    $CollisionShape2D.set_deferred("disabled", true)
+    
