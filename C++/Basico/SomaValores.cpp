37d1c948bf84520f1b51256ebcb0df5c8d8b2ba7
Arthur Siqueira <arthur@oneweb.com.br>
2021-02-19 21:14:20 -0300

    Adicionado exemplo de input e soma de valores em C++

---
+++ C++/Basico/SomaValores.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 0,
+        b = 0,
+        soma = 0;
+
+    cout << "Qual é o primeiro número? ";
+    cin >> a;
+    cout << "Qual é o segundo número? ";
+    cin >> b;
+    soma = a + b;
+    cout << "Soma = " << soma << endl;
+
+    return 0;
+}
