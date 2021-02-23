c3e230bf0d4d326577de28d7956a4524f95d2273
Arthur Siqueira <arthur.siq5@gmail.com>
2021-02-23 08:08:18 -0300

    Adicionados exemplos de uso de sets

---
+++ Python3/Listas/SetsEx1.py
@@ -0,0 +1,7 @@
+num_set = {1, 2, 3, 4, 5}
+
+# sets são semelhantes a listas e dicionários, mas são não-ordenados
+# isso significa que sets não podem ser indexados
+# devido a sua natureza não ordenada, são muito mais rápidos de se
+#identificar a existência de um elemento usando o operador "in"
+print(3 in num_set)
