99d44adb293438289f903e988f85c2f071375a41
Arthur Siqueira <arthur.siq5@gmail.com>
2021-02-23 08:12:04 -0300

    Adicionados exemplos de adição e remoção de elementos de um set

--- Python3/Listas/SetsEx2.py
+++ Python3/Listas/SetsEx2.py
@@ -1,4 +1,21 @@
 letters = {"a", "b", "c", "d"}
+
+if "e" not in letters:
+    print(1)
+else: 
+    print(2)
+
+# você pode adicionar elementos usando a função "add" dos sets
+letters.add("e")
+
+if "e" not in letters:
+    print(1)
+else: 
+    print(2)
+
+# da mesma forma, pode remover elementos usando a função "remove"
+letters.remove("e")
+
 if "e" not in letters:
     print(1)
 else: 
