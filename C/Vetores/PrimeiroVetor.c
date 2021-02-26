a67d4621963a89735b4775cc3bc417500e829dea
Arthur Siqueira <arthur.siq5@gmail.com>
2021-02-25 22:25:42 -0300

    Primeiro exemplo de vetor em  C

---
+++ C/Vetores/PrimeiroVetor.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    int vetor[5] = {17, 42, 9, 33, 12};
+
+    printf("[%d, %d, %d, %d, %d]\n",
+            vetor[0],
+            vetor[1],
+            vetor[2],
+            vetor[3],
+            vetor[4]
+    );
+
+    return 0;
+
+}
