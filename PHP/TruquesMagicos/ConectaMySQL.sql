5261599f62afcd3b6366b3f8f10d8de72ab5c19f
Arthur Siqueira <arthur.siq5@gmail.com>
2021-03-08 21:53:55 -0300

    Corrigida criação da tabela

--- PHP/TruquesMagicos/ConectaMySQL.sql
+++ PHP/TruquesMagicos/ConectaMySQL.sql
@@ -1,7 +1,8 @@
 CREATE TABLE modulos (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(255)
+    nome VARCHAR(255),
+    categoria VARCHAR(32)
 );
 
-INSERT INTO modulos (nome)
-    VALUES ('Geografia');
+INSERT INTO modulos (nome, categoria)
+    VALUES ('Geografia', 'Ensino Médio');
