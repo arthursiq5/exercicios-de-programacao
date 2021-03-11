5b62f18ff54b8645d53c5a9eb276f95659c2dcdc
Arthur Siqueira <arthur.siq5@gmail.com>
2021-03-10 23:13:13 -0300

    Adicionado exemplo de uso de generators

---
+++ Python3/Listas/GeneratorEx1.py
@@ -0,0 +1,10 @@
+def countdown():
+    i=5
+    while i > 0:
+        yield i # o comando "yield" retornará um tipo de lista a cada iteração
+        i -= 1
+
+for i in countdown():
+    print(i) # essa lista pode ser mostrada dessa forma
+
+print(countdown()) # isso retornará uma posição na memória
