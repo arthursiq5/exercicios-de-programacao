8ccbaee1b30275cc3a490da1eaf344324e798663
Arthur Siqueira <arthur.siq5@gmail.com>
2021-03-15 08:27:51 -0300

    Adicionados indicadores de direção do personagem

--- HTML5/ProgramacaoGrafica/WolfClone/src/index.js
+++ HTML5/ProgramacaoGrafica/WolfClone/src/index.js
@@ -4,6 +4,9 @@ import { gameHeight, gameWidth } from './constantes';
 
 const containerElement = document.getElementById('p5-container');
 
+let position;
+let direction;
+
 const sketch = (p) => {
     let x = 100;
     let y = 100;
@@ -11,6 +14,8 @@ const sketch = (p) => {
     p.setup = function() {
         p.createCanvas(gameWidth, gameHeight);
         p.noStroke();
+        position = p.createVector(5, 5);
+        direction = p.createVector(0, -1);
     };
 
     p.draw = function() {
