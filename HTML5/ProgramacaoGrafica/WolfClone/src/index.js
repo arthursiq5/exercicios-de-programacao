950dcbd780ae9f31c117869e9f4ed045bf78e00a
Arthur Siqueira <arthur.siq5@gmail.com>
2021-03-15 08:11:32 -0300

    Criadas definições básicas do jogo

--- HTML5/ProgramacaoGrafica/WolfClone/src/index.js
+++ HTML5/ProgramacaoGrafica/WolfClone/src/index.js
@@ -3,16 +3,22 @@ import { gamemap } from './Mapa';
 
 const containerElement = document.getElementById('p5-container');
 
+const gameWidth = 320;
+const gameHeight = 200;
+
 const sketch = (p) => {
     let x = 100;
     let y = 100;
 
     p.setup = function() {
-        p.createCanvas(800, 800);
+        p.createCanvas(gameWidth, gameHeight);
+        p.noStroke();
     };
 
     p.draw = function() {
-        p.background(255);
+        p.background(190, 190, 255);
+        p.fill(130);
+        p.rect(0, gameHeight/2, gameWidth, gameHeight/2)
     };
 };
 
