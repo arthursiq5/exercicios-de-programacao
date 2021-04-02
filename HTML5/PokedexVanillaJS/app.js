1b8fc747c5cd13318e645abfc31d17e2edf86ba3
Arthur Siqueira <arthur.siq5@gmail.com>
2021-04-02 00:48:15 -0300

    Puxados dados da promise e realizadas requisições e resoluções de requisições de forma assíncrona

--- HTML5/PokedexVanillaJS/app.js
+++ HTML5/PokedexVanillaJS/app.js
@@ -2,12 +2,18 @@ const fetchPokemon = () => {
     const totalPokemons = 150;
     const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`;
 
+    const pokemonPromises = [];
+
     for (let id = 1; id <= totalPokemons; id++) {
-        fetch(getPokemonUrl(id))
-            .then(response => response.json());    
+        pokemonPromises.push(
+            fetch(getPokemonUrl(id))
+                .then(response => response.json()));
     }
 
-    
+    Promise.all(pokemonPromises)
+        .then(pokemons => {
+            console.log(pokemons)
+        })
 };
 
 fetchPokemon();
