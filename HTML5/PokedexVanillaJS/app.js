2f3bd9318aa45a70dd36eef9b5c66800f56dde3b
Arthur Siqueira <arthur.siq5@gmail.com>
2021-04-02 17:18:52 -0300

    Melhorada exibição de pokemons

--- HTML5/PokedexVanillaJS/app.js
+++ HTML5/PokedexVanillaJS/app.js
@@ -16,8 +16,8 @@ const fetchPokemon = () => {
             const lisPokemons = pokemons.reduce((accumulator, pokemon) => {
                 const types = pokemon.types.map(typeInfo => typeInfo.type.name)
 
-                accumulator += `<li class="card">
-                    <img class="card-image ${types[0]}" alt="${pokemon.name}" src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png">
+                accumulator += `<li class="card ${types[0]}">
+                    <img class="card-image" alt="${pokemon.name}" src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png">
                     <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
                     <p class="card-subtitle">${types.join(' | ')}</p>
                 </li>`;
