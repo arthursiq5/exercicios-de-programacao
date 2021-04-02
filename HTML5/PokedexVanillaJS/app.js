f03d58e928cc715c263a364067f4adf7a7f77574
Arthur Siqueira <arthur.siq5@gmail.com>
2021-04-02 17:18:01 -0300

    Exibidos dados e imagens dos pokemons na tela inicial

--- HTML5/PokedexVanillaJS/app.js
+++ HTML5/PokedexVanillaJS/app.js
@@ -12,20 +12,21 @@ const fetchPokemon = () => {
 
     Promise.all(pokemonPromises)
         .then(pokemons => {
-            // console.log(pokemons)
 
             const lisPokemons = pokemons.reduce((accumulator, pokemon) => {
                 const types = pokemon.types.map(typeInfo => typeInfo.type.name)
 
                 accumulator += `<li class="card">
-                    <img class="card-image ${types[0]}" alt="${pokemon.name}" src="">
+                    <img class="card-image ${types[0]}" alt="${pokemon.name}" src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png">
                     <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
                     <p class="card-subtitle">${types.join(' | ')}</p>
                 </li>`;
                 return accumulator;
             }, '');
-            console.log(lisPokemons);
-        })
+
+            const ul = document.querySelector('ul[data-js="pokedex"].pokedex');
+            ul.innerHTML = lisPokemons;
+        });
 };
 
 fetchPokemon();
