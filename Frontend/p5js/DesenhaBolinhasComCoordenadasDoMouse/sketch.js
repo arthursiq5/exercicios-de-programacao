848933c46c5111b95cbce2252dead754f3faac6a
Arthur Siqueira <arthur.siq5@gmail.com>
2021-04-04 16:45:30 -0300

    Pintando bolinhas na tela conforme a posição do mouse

---
+++ Frontend/p5js/DesenhaBolinhasComCoordenadasDoMouse/sketch.js
@@ -0,0 +1,8 @@
+function setup() {
+    createCanvas(400, 400); // cria um canvas de 400px por 400px
+    background(220); // define a cor de fundo do canvas
+}
+
+function draw() {
+    circle(mouseX, mouseY, 50);
+}
