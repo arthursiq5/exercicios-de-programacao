461ab1aade885dcac543cbc44c558f1bf5784015
Arthur Siqueira <arthur.siq5@gmail.com>
2021-04-04 18:41:29 -0300

    Criado campo do jogo

--- Frontend/p5js/Pong/js/sketch.js
+++ Frontend/p5js/Pong/js/sketch.js
@@ -1,6 +1,28 @@
+var dots = [];
+const dSize = 10;
+
 function setup() {
-    
+    createCanvas(800, 500);
+
+    for (let y = dSize / 2; y < height; y += dSize * 2) {
+        dots.push(createVector((width / 2) - (dSize / 2), y));
+    }
 }
 
 function draw() {
+    background(0);
+
+    noStroke();
+    fill(255);
+    drawSquares();
+}
+
+function drawSquares(){
+    let dotsLength = dots.length;
+    for(let i = 0; i < dotsLength; i++){
+        let x = dots[i].x;
+        let y = dots[i].y;
+
+        rect(x, y, dSize, dSize);
+    }
 }
