d2f34de426abacabf64ebfb36adba5f7ae1316fb
Arthur Siqueira <arthur.siq5@gmail.com>
2021-04-14 08:32:21 -0300

    Criado exemplo de uso do PhpUnit para testes automatizados

---
+++ PHP/TestesAutomatizados/Introducao/tests/ExemploTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class ExemploTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function testExemplo()
+    {
+        $this->assertTrue(true);
+    }
+}
