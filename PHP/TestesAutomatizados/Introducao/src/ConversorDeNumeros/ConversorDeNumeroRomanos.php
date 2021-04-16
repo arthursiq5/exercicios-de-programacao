ad196d31074eba8e61939b9296eb013fbd0ea1ac
Arthur Siqueira <arthur.siq5@gmail.com>
2021-04-15 22:03:01 -0300

    Terminado teste e conversão dos símbolos

--- PHP/TestesAutomatizados/Introducao/src/ConversorDeNumeros/ConversorDeNumeroRomanos.php
+++ PHP/TestesAutomatizados/Introducao/src/ConversorDeNumeros/ConversorDeNumeroRomanos.php
@@ -4,8 +4,22 @@ namespace App\ConversorDeNumeros;
 
 class ConversorDeNumeroRomanos
 {
+    protected array $tabela = [
+        'I' => 1,
+        'V' => 5,
+        'X' => 10,
+        'L' => 50,
+        'C' => 100,
+        'D' => 500,
+        'M' => 1000
+    ];
+
     public function converte(string $numeroRomano): int
     {
+        if (array_key_exists($numeroRomano, $this->tabela)) {
+            return $this->tabela[$numeroRomano];
+        }
+
         return 0;
     }
 }
--- PHP/TestesAutomatizados/Introducao/tests/ConversorDeNumeros/ConversorDeNumeroRomanosTest.php
+++ PHP/TestesAutomatizados/Introducao/tests/ConversorDeNumeros/ConversorDeNumeroRomanosTest.php
@@ -7,10 +7,15 @@ use PHPUnit\Framework\TestCase;
 
 class ConversorDeNumeroRomanosTest extends TestCase
 {
-    public function testDeveEntenderOSimboloI()
+    public function testDeveEntenderOsSimbolosRomanos()
     {
         $romano = new ConversorDeNumeroRomanos();
-        $numero = $romano->converte('I');
-        $this->assertEquals(1, $numero);
+        $this->assertEquals(1, $romano->converte('I'));
+        $this->assertEquals(5, $romano->converte('V'));
+        $this->assertEquals(10, $romano->converte('X'));
+        $this->assertEquals(50, $romano->converte('L'));
+        $this->assertEquals(100, $romano->converte('C'));
+        $this->assertEquals(500, $romano->converte('D'));
+        $this->assertEquals(1000, $romano->converte('M'));
     }
 }
