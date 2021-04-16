199528a40ad7649bbfca5761c5a0681d6b729a81
Arthur Siqueira <arthur.siq5@gmail.com>
2021-04-16 18:43:09 -0300

    Listados produtos da base no controller

--- PHP/Laravel/estoque/app/Http/Controllers/ProdutoController.php
+++ PHP/Laravel/estoque/app/Http/Controllers/ProdutoController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
+
 class ProdutoController extends Controller{
 
     public function lista() {
-        return '<h1>Listagem de produtos com Laravel</h1>';
+
+        $html = '<h1>Listagem de produtos com Laravel</h1>';
+        $html .= '<ul>';
+
+        $produtos = DB::select('SELECT * FROM produtos');
+        foreach ($produtos as $produto) {
+            $html .= '<li> Nome: ' . $produto->nome
+                . ', Descrição: ' . $produto->descricao . '</li>';
+        }
+        $html .= '</ul>';
+
+        return $html;
     }
 }
