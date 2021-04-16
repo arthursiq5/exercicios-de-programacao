4d425b24571fc743ae3871b6b1dd4273f52956ca
Arthur Siqueira <arthur.siq5@gmail.com>
2021-04-15 22:01:06 -0300

    Adicionados testes para arrays

--- PHP/TestesAutomatizados/Introducao/tests/BasicTest.php
+++ PHP/TestesAutomatizados/Introducao/tests/BasicTest.php
@@ -15,4 +15,11 @@ class BasicTest extends TestCase
     {
         $this->assertTrue(true);
     }
+
+    public function testOperacoesComArray()
+    {
+        $this->assertEquals([], array());
+        $this->assertEquals([1, 2, 3], array(1, 2, 3));
+        $this->assertEquals(['nome' => 'fulano', 'idade' => 25], ['nome' => 'fulano', 'idade' => 25]);
+    }
 }
