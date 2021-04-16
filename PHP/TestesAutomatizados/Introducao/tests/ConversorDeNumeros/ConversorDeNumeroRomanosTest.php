76932e88c2e9ae7f0de067a223000550ac0a66e7
Arthur Siqueira <arthur.siq5@gmail.com>
2021-04-15 22:33:25 -0300

    Poupada memória ao instanciar objeto apenas uma vez

--- PHP/TestesAutomatizados/Introducao/tests/ConversorDeNumeros/ConversorDeNumeroRomanosTest.php
+++ PHP/TestesAutomatizados/Introducao/tests/ConversorDeNumeros/ConversorDeNumeroRomanosTest.php
@@ -7,23 +7,29 @@ use PHPUnit\Framework\TestCase;
 
 class ConversorDeNumeroRomanosTest extends TestCase
 {
+    private ConversorDeNumeroRomanos $romano;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->romano = new ConversorDeNumeroRomanos();
+    }
+
     public function testDeveEntenderOsSimbolosRomanos()
     {
-        $romano = new ConversorDeNumeroRomanos();
-        $this->assertEquals(1, $romano->converte('I'));
-        $this->assertEquals(5, $romano->converte('V'));
-        $this->assertEquals(10, $romano->converte('X'));
-        $this->assertEquals(50, $romano->converte('L'));
-        $this->assertEquals(100, $romano->converte('C'));
-        $this->assertEquals(500, $romano->converte('D'));
-        $this->assertEquals(1000, $romano->converte('M'));
+        $this->assertEquals(1, $this->romano->converte('I'));
+        $this->assertEquals(5, $this->romano->converte('V'));
+        $this->assertEquals(10, $this->romano->converte('X'));
+        $this->assertEquals(50, $this->romano->converte('L'));
+        $this->assertEquals(100, $this->romano->converte('C'));
+        $this->assertEquals(500, $this->romano->converte('D'));
+        $this->assertEquals(1000, $this->romano->converte('M'));
     }
 
     public function testDeveEntenderSimbolosDuplicados()
     {
-        $romano = new ConversorDeNumeroRomanos();
-        $this->assertEquals(2, $romano->converte('II'));
-        $this->assertEquals(3, $romano->converte('III'));
-        $this->assertEquals(7, $romano->converte('VII'));
+        $this->assertEquals(2, $this->romano->converte('II'));
+        $this->assertEquals(3, $this->romano->converte('III'));
+        $this->assertEquals(7, $this->romano->converte('VII'));
     }
 }
