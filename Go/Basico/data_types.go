49be7317dbd05c51a4e4ed2da44058bda45e56d8
Arthur Siqueira <arthur.siq5@gmail.com>
2021-05-04 18:27:42 -0300

    Adicionado exemplo dos tipos de dados em Go

---
+++ Go/Basico/data_types.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x int = 42
+	var y float32 = 1.37
+	var name string = "James"
+	var online bool = true
+
+	fmt.Println(name)
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(online)
+}
