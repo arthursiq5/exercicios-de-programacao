7ace900c51f6fb5a2dd38c0522bc0333a922c3b9
Arthur Siqueira <arthur.siq5@gmail.com>
2021-05-05 18:37:35 -0300

    Melhorada tipagem

--- PHP/RegEx/src/BasicRegEx.php
+++ PHP/RegEx/src/BasicRegEx.php
@@ -3,7 +3,8 @@
 namespace App;
 
 abstract class BasicRegEx {
-    function assert(string $regex, string $value){
+    function assert(string $regex, string $value): bool
+    {
         return !empty(preg_match($regex, $value));
     }
 }
--- PHP/RegEx/src/FirstRegEx.php
+++ PHP/RegEx/src/FirstRegEx.php
@@ -3,7 +3,8 @@
 namespace App;
 
 class FirstRegEx extends BasicRegEx {
-    function separatedByCommas(string $value){
+    function separatedByCommas(string $value):bool
+    {
         return $this->assert('/(.*), (.*)/', $value);
     }
 }
