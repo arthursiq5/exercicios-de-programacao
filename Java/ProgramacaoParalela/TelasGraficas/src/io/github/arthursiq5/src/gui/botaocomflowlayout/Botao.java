9aae6f5a7d9f67a38ab335812910e64c4968b3c8
Arthur Siqueira <arthur.siq5@gmail.com>
2021-05-18 22:31:03 -0300

    Adicionado layout para os botões

---
+++ Java/ProgramacaoParalela/TelasGraficas/src/io/github/arthursiq5/src/gui/botaocomflowlayout/Botao.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.github.arthursiq5.src.gui.botaocomflowlayout;
+
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author arthur
+ */
+public class Botao extends JFrame {
+
+    private JButton ok;
+    private JButton cancelar;
+
+    public Botao() {
+        super("Criando botões");
+        this.setLayout(new FlowLayout());
+
+        this.ok = new JButton("Ok");
+        this.cancelar = new JButton("Cancelar");
+        
+        this.add(this.ok);
+        this.add(this.cancelar);
+    }
+}
