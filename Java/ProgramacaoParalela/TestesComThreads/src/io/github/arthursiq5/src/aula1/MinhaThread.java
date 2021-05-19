bf344042adccfe973d8055f267837e4ceaeec644
Arthur Siqueira <arthur.siq5@gmail.com>
2021-05-18 23:29:50 -0300

    Adicionado exemplo de uso de threads

---
+++ Java/ProgramacaoParalela/TestesComThreads/src/io/github/arthursiq5/src/aula1/MinhaThread.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.github.arthursiq5.src.aula1;
+
+/**
+ *
+ * @author arthur
+ */
+public class MinhaThread extends Thread {
+
+    private String nome;
+
+    public MinhaThread(String nome) {
+        this.nome = nome;
+        this.start();
+    }
+
+    @Override
+    public void run() {
+        try {
+            for (int i = 0; i < 6; i++) {
+                System.out.println(this.nome + " - contador " + i);
+                Thread.sleep(100);
+            }
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
---
+++ Java/ProgramacaoParalela/TestesComThreads/src/io/github/arthursiq5/src/aula1/MinhaThreadTeste.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.github.arthursiq5.src.aula1;
+
+/**
+ *
+ * @author arthur
+ */
+public class MinhaThreadTeste {
+
+    public static void main(String[] args) {
+        MinhaThread thread1 = new MinhaThread("Thread #1");
+        MinhaThread thread2 = new MinhaThread("Thread #2");
+    }
+}
