951aaf152594658859fa8b7cbbb1c4aa669a5e76
Arthur Siqueira <arthur.siq5@gmail.com>
2021-05-19 21:40:23 -0300

    Corrigida função para passar pelos testes

--- PHP/TestesAutomatizados/src/Loja/RH/CalculadoraDeSalario.php
+++ PHP/TestesAutomatizados/src/Loja/RH/CalculadoraDeSalario.php
@@ -8,10 +8,10 @@ class CalculadoraDeSalario
     {
         if ($funcionario->getCargo() === TabelaCargos::DESENVOLVEDOR) {
             if ($funcionario->getSalario() > 3000) {
-                return 3200.0;
+                return $funcionario->getSalario() * 0.8;
             }
-            return 1350.0;
+            return $funcionario->getSalario() * 0.9;
         }
-        return 425.0;
+        return $funcionario->getSalario() * 0.85;
     }
 }
