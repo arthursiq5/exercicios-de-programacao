f19d17768a7c280147064c7c8a2838df0c2ef5ce
Arthur Siqueira <arthur.siq5@gmail.com>
2021-05-22 22:49:25 -0300

    Criada função da classe

--- Python3/POO/PrimeiraClasse.py
+++ Python3/POO/PrimeiraClasse.py
@@ -2,8 +2,12 @@ class Cat:
     def __init__(self, color, legs):
         self.color = color
         self.legs = legs
+    
+    def sound(self):
+        print('Meow')
 
 felix = Cat('Siamês', 4)
 print('Felix { Cor:', felix.color, ', Patas:', felix.legs, '}')
+felix.sound()
 tigre = Cat('Laranja', 4)
 print('Tigre { Cor:', tigre.color, ', Patas:', tigre.legs, '}')
