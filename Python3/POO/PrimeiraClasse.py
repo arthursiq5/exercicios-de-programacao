e353ee63e46f5b39ce670249087a482897830bd5
Arthur Siqueira <arthur.siq5@gmail.com>
2021-05-22 22:43:19 -0300

    Criada primeira classe em Python

---
+++ Python3/POO/PrimeiraClasse.py
@@ -0,0 +1,9 @@
+class Cat:
+    def __init__(self, color, legs):
+        self.color = color
+        self.legs = legs
+
+felix = Cat('Siamês', 4)
+print('Felix { Cor:', felix.color, ', Patas:', felix.legs, '}')
+tigre = Cat('Laranja', 4)
+print('Tigre { Cor:', tigre.color, ', Patas:', tigre.legs, '}')
