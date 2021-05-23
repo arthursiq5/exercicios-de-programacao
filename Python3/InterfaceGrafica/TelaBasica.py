5703c9617d40d6cd6a5ba40a7b2df3081e83c7f7
Arthur Siqueira <arthur.siq5@gmail.com>
2021-05-23 00:23:16 -0300

    Adaptada GUI para trabalhar com POO

--- Python3/InterfaceGrafica/TelaBasica.py
+++ Python3/InterfaceGrafica/TelaBasica.py
@@ -1,4 +1,8 @@
 import tkinter
 
-main_window = tkinter.Tk()
-tkinter.mainloop()
+class MinhaGUI:
+    def __init__(self):
+        self.main_window = tkinter.Tk()
+        tkinter.mainloop()
+
+minha_gui = MinhaGUI()
